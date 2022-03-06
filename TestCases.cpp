53973e1dcbd4994b8af8ebb18395c4150287e96b
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-06 17:59:52 +0530

    Update TestCases.cpp

--- TestCases.cpp
+++ TestCases.cpp
@@ -1,1 +1,11 @@
+#define CATCH_CONFIG_MAIN 
 
+#include "test/catch.hpp"
+#include "RangeSort.h"
+#include "RangeCheck.h"
+
+// First test before starting the coding : to check range of 2 consecutive current samples
+TEST_CASE("Test 1 : Check range for 2 consecutive current samples") {
+  int CurrentSamples[] = {4,5};
+  REQUIRE(CheckCurrentSamplesRange(CurrentSamples, 2) == 1);
+}

f469b9a0b59b89d980a9802b97e33c933d9aa83e
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-07 21:23:18 +0530

    Update RangeSort.c

--- RangeSort.c
+++ RangeSort.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "RangeSort.h"
 
+void SortInputArray(int Array[], int NumOfElements)
+{
+    int Index1, KeyValue, Index2;
+    for (Index1 = 1; Index1 < NumOfElements; Index1++)
+    {
+        KeyValue = Array[Index1];
+        Index2 = Index1 - 1;
+        while (Index2 >= 0 && Array[Index2] > KeyValue)
+        {
+            Array[Index2 + 1] = Array[Index2];
+            Index2 = Index2 - 1;
+        }
+        Array[Index2 + 1] = KeyValue;
+    }
+}

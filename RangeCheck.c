c91375f8ed1f761c5b1e31ebca1a79d9cce26822
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-07 21:25:19 +0530

    Update RangeCheck.c

--- RangeCheck.c
+++ RangeCheck.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "RangeCheck.h"
+#include "RangeSort.h"
 
 int CheckCurrentSamplesRange (int *CurrentSamples , int NumOfSamples)
 {
   int DifferenceBetweenSamples = 0;
   int NumConsecutiveRange = 0;
   int index = 0;
+  SortInputArray(CurrentSamples,NumOfSamples);
   for(index = 0; index < NumOfSamples; index++)
   {
     DifferenceBetweenSamples = CurrentSamples[index + 1] - CurrentSamples[index];

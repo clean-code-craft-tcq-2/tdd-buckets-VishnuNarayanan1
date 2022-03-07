c5ed84aef3b6a08a75354cd5b2e3a63cf1a7bd9d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-07 20:53:27 +0530

    Update TestCases.cpp

--- TestCases.cpp
+++ TestCases.cpp
@@ -4,8 +4,23 @@
 #include "RangeSort.h"
 #include "RangeCheck.h"
 
+//************************************************
+// Go through test cases for the adapted TDD flow :
+//************************************************
+
 // Test 1 : before starting the coding
+/*
 TEST_CASE("Test 1 : Check range for 2 consecutive current samples") {
   int CurrentSamples[] = {4,5};
   REQUIRE(CheckCurrentSamplesRange(CurrentSamples, 2) == 1);
 }
+*/
+
+
+// Test 2 : defined CheckCurrentSamplesRange with always return 0 , no change in the test case
+/*
+TEST_CASE("Test 2 : Check range for 2 consecutive current samples") {
+  int CurrentSamples[] = {4,5};
+  REQUIRE(CheckCurrentSamplesRange(CurrentSamples, 2) == 1);
+}
+*/

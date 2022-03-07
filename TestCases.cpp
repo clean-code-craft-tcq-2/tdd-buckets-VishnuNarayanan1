d00b14faf70f7625fc844162d8b6a807c2c3368f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-07 22:09:33 +0530

    Update TestCases.cpp

--- TestCases.cpp
+++ TestCases.cpp
@@ -52,11 +52,23 @@ TEST_CASE("Test 5 : Check range for 3 current samples with first and last input 
 
 // Test 6 : Improved the code by incrementing the range count for standalone numbers which doesn't form any range.
 //          For example in the following input {4,10,5} , 4 and 5 forms a continuous range but 10 is a standalone number.
-//          no change in the test case : PASSED
+//          no change in the test case : EXPECTED TO BE PASSED, BUT FAILED : But found a bug : 
+//          for loop needs to be run with condition " < NUM_SAMPLES - 1" as we process "SAMPLE + 1" while indexing.
 
+/*
 TEST_CASE("Test 6 : Check range for 3 current samples with first and last input being consecutive") {
   int CurrentSamples[] = {4,10,5};
   REQUIRE(CheckCurrentSamplesRange(CurrentSamples, 3) == 2);
 }
+*/
+
+// Test 7 : Corrected the loop condition bug found in Test 6 , no change in the test case : PASSED
+
+/*
+TEST_CASE("Test 7 : Check range for 3 current samples with first and last input being consecutive") {
+  int CurrentSamples[] = {4,10,5};
+  REQUIRE(CheckCurrentSamplesRange(CurrentSamples, 3) == 2);
+}
+*/
 
 

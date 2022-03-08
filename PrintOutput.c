5daeff152684b4039c48fc4d2361b40c5af961ff
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-08 08:59:03 +0530

    Update PrintOutput.c

--- PrintOutput.c
+++ PrintOutput.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "PrintOutput.h"
 
+#ifdef PRODUCTION_ENVIRONMENT
 void PrintOutput(int MinVal, int MaxVal, int NumRange)
 {
   printf("\n%d - %d , %d", MinVal, MaxVal, NumRange);
 }
+#endif
+
+

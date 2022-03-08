a19fe69aae6abfdb6dcb265f80eb9dccc1c2410b
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-08 07:23:26 +0530

    Update PrintOutput.c

--- PrintOutput.c
+++ PrintOutput.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "PrintOutput.h"
 
-void Printoutput(int MinVal, int MaxVal, int NumRange)
+void PrintOutput(int MinVal, int MaxVal, int NumRange)
 {
   printf("%d - %d , %d", MinVal, MaxVal, NumRange);
 }

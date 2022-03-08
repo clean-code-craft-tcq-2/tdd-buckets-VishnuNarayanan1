1070d8f22282030f59d965956de974240a1de10c
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-03-08 08:32:17 +0530

    Update RangeSort.c

--- RangeSort.c
+++ RangeSort.c
@@ -8,7 +8,7 @@ void SortInputArray(int Array[], int NumOfElements)
     {
         KeyValue = Array[Index1];
         Index2 = Index1 - 1;
-        while (Index2 >= 0 && Array[Index2] > KeyValue)
+        while (Array[Index2] > KeyValue)
         {
             Array[Index2 + 1] = Array[Index2];
             Index2 = Index2 - 1;
